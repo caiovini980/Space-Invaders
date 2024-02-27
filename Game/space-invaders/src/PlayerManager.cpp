403696d0bed5a41840288afabb800b9792ee9f18
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2024-02-27 18:11:16 -0300

    feat: creating the player and get it's inputs

--- Game/space-invaders/src/PlayerManager.cpp
+++ Game/space-invaders/src/PlayerManager.cpp
@@ -1,1 +1,52 @@
 ﻿#include "PlayerManager.h"
+
+#include <GLFW/glfw3.h>
+
+#include "ResourceManager.h"
+
+PlayerManager::PlayerManager()
+{
+}
+
+PlayerManager::~PlayerManager()
+{ }
+
+void PlayerManager::CreatePlayer(float screenWidth, float screenHeight)
+{
+    std::cout << "Creating player\n";
+    
+    const std::string playerTextureName = "Player";
+    ResourceManager::LoadTexture("res/textures/awesomeface.png", playerTextureName, true);
+    
+    m_PlayerSprite = ResourceManager::GetTexture(playerTextureName);
+    
+    const glm::vec2 playerInitialPosition = glm::vec2(
+        (screenWidth / 2.0f) - (PLAYER_SIZE.x / 2.0f),
+        screenHeight - PLAYER_SIZE.y);
+    
+    m_Player = std::make_shared<GameObject>(
+        playerInitialPosition,
+        PLAYER_SIZE,
+        m_PlayerSprite,
+        glm::vec3(1.0f),
+        glm::vec2(PLAYER_SPEED)
+        );
+}
+
+void PlayerManager::ProcessInput(float deltaTime, const Input& input)
+{
+    if (input.GetKey(GLFW_KEY_D))
+    {
+        std::cout << "Moving right!\n";
+    }
+    
+    else if (input.GetKey(GLFW_KEY_A))
+    {
+        std::cout << "Moving left!\n";
+    }
+}
+
+void PlayerManager::Render(SpriteRenderer& renderer) const
+{
+    renderer.Draw(*m_PlayerSprite, m_Player->Position, m_Player->Size, m_Player->Rotation, m_Player->Color);
+}
--- Game/space-invaders/src/PlayerManager.h
+++ Game/space-invaders/src/PlayerManager.h
@@ -1,7 +1,27 @@
 ﻿#pragma once
+#include "GameObject.h"
+
+#include <iostream>
+
+#include "glm/glm.hpp"
+
+#include "Input.h"
+#include "SpriteRenderer.h"
 
 class PlayerManager
 {
 public:
+    PlayerManager();
+    ~PlayerManager();
+
+    void CreatePlayer(float screenWidth, float screenHeight);
+    void ProcessInput(float deltaTime, const Input& input);
+    void Render(SpriteRenderer& renderer) const;
+
+private:
+    const glm::vec2 PLAYER_SIZE = glm::vec2(100.0f, 80.0f);
+    const float PLAYER_SPEED = 500.0f;
     
+    std::shared_ptr<GameObject> m_Player;
+    std::shared_ptr<Texture> m_PlayerSprite;
 };
